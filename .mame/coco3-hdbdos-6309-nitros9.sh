dda21cd94067490aaf0c0995ee1f0bae5bedb271
Ron Klein <ron@kdomain.org>
2021-08-02 19:40:08 -0500

    updated CoCo 3 scripts

---
+++ .mame/coco3-hdbdos-6309-nitros9.sh
@@ -0,0 +1,21 @@
+clear
+
+MAMEPARMSFILE=`cat $HOME/.mame/.optional_mame_parameters.txt`
+export MAMEPARMS=$MAMEPARMSFILE
+
+mame coco3h -ramsize 2048k -ext multi -flop1 /media/share1/EMU/EOU/63EMU.dsk -hard1 /media/share1/EMU/EOU/63SDC.VHD -autoboot_delay 2 -autoboot_command 'DOS\n' $MAMEPARMS
+
+# capture MAME ERRORLEVEL
+
+if [ $? -eq 0 ]
+then
+        echo
+else
+        echo
+        echo "Please make note of message above when requesting help."
+        echo
+        read -p  "Press any key to continue." -n1 -s
+fi
+
+cd $HOME/.mame
+CoCoPi-menu-Coco3.sh
