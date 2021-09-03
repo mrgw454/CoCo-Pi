93a843d1daaf12e88fc8c7babf502691a1a78ad1
Ron Klein <ron@kdomain.org>
2021-09-03 08:57:58 -0500

    add Squanchy-BASIC DSK image from Tim Lindner

--- scripts/fix-cocopi.sh
+++ scripts/fix-cocopi.sh
@@ -134,6 +134,21 @@ else
 fi
 
 
+# add Squanchy BASIC from Tim Lindner
+# check for fix
+fix="fix-20210903-01"
+if grep -q "$fix" $file; then
+	echo fix $fix already complete.
+	echo
+else
+	echo Applying fix $fix...
+	echo
+	unzip -o -j $HOME/update/Squanchy.zip -d /media/share1/SDC/TLINDNER
+	echo "$fix" >>$file
+	echo
+fi
+
+
 echo
 
 echo
