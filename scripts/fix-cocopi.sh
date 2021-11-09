ce2ed53a4cb8f4df22ecbef60f571ff30a324ce7
Ron Klein <ron@kdomain.org>
2021-11-09 09:06:32 -0600

    add vbcc compiler for RPi

--- scripts/fix-cocopi.sh
+++ scripts/fix-cocopi.sh
@@ -552,6 +552,21 @@ else
 fi
 
 
+# add vbcc compiler
+# check for fix
+fix="fix-20211109-01"
+if grep -q "$fix" $file; then
+        echo fix $fix already complete.
+        echo
+else
+        echo Applying fix $fix...
+        echo
+        tar xzf /home/pi/update/vbcc-20210903.tar.gz -C /
+        echo "$fix" >>$file
+        echo
+fi
+
+
 
 echo
 
