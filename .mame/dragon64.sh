f706e80298488685d769b60a5eb7c6637da7cc14
Ron Klein <ron@kdomain.org>
2021-11-22 10:17:18 -0600

    update some MAME scripts

---
+++ .mame/dragon64.sh
@@ -0,0 +1,50 @@
+clear
+
+MAMEPARMSFILE=`cat $HOME/.mame/.optional_mame_parameters.txt`
+export MAMEPARMS=$MAMEPARMSFILE
+
+if [ -e $HOME/.mame/.mame_floppy ]
+then
+    floppy=`cat $HOME/.mame/.mame_floppy`
+
+    if [ -e "$floppy" ]
+    then
+
+        echo "Floppy save file found - automounting \"$floppy\""
+        echo
+        read -p  "Press any key to continue." -n1 -s
+        echo
+
+	mame dragon64 -ramsize 64k -flop1 "$floppy" $MAMEPARMS
+
+    else
+
+       echo "Floppy disk image [$floppy] not found in floppy save file.  Aborting."
+        echo
+        echo "Please run \"Clear all saved mount files\" on Utilities Menu."
+        echo
+        read -p  "Press any key to continue." -n1 -s
+        echo
+
+    fi
+
+else
+
+    mame dragon64 -ramsize 64k -flop1 "$1" $MAMEPARMS
+
+fi
+
+# capture MAME ERRORLEVEL
+
+if [ $? -eq 0 ]
+then
+        echo
+else
+        echo
+        echo "Please make note of message above when requesting help."
+        echo
+        read -p  "Press any key to continue." -n1 -s
+fi
+
+cd $HOME/.mame
+CoCoPi-menu-Dragon.sh
