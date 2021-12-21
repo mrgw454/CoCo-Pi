66a14235f4fb0373ce73da8dd22083c879e14bbd
Ron Klein <ron@kdomain.org>
2021-12-20 18:49:22 -0600

    add new CoCo carts

--- scripts/fix-cocopi.sh
+++ scripts/fix-cocopi.sh
@@ -793,6 +793,21 @@ else
 fi
 
 
+# add new public domain CoCo cart files
+# check for fix
+fix="fix-20211220-01"
+if grep -q "$fix" $file; then
+        echo fix $fix already complete.
+        echo
+else
+        echo Applying fix $fix...
+        echo
+        cp $HOME/update/*.ccc /media/share1/carts
+        echo "$fix" >>$file
+        echo
+fi
+
+
 
 echo
 
