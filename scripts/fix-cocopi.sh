d50379ae874ad9df1e13cf1c4a4dd91fcc62e369
Ron Klein <ron@kdomain.org>
2022-01-31 10:28:19 -0600

    add new dev library for mouse support

--- scripts/fix-cocopi.sh
+++ scripts/fix-cocopi.sh
@@ -902,6 +902,21 @@ else
 fi
 
 
+# add new packages
+# check for fix
+fix="fix-20220131-01"
+if grep -q "$fix" $file; then
+        echo fix $fix already complete.
+        echo
+else
+        echo Applying fix $fix...
+        echo
+        sudo apt install -y libxpm-dev
+        echo "$fix" >>$file
+        echo
+fi
+
+
 
 echo
 
