46c6000846c52f831b8ec68a2c9fad335fc47d93
Ron Klein <ron@kdomain.org>
2023-02-06 07:20:30 -0600

    add latest CMOC package

--- scripts/fix-cocopi.sh
+++ scripts/fix-cocopi.sh
@@ -1233,6 +1233,24 @@ else
 fi
 
 
+# add new cmoc package
+# check for fix
+fix="fix-20230206-01"
+if grep -q "$fix" $file; then
+        echo fix $fix already complete.
+        echo
+else
+        echo Applying fix $fix...
+        echo
+        tar xvf $HOME/update/cmoc-0.1.81-CoCoPi.tar.gz -C /
+        cd $HOME/source/cmoc-0.1.81
+        sudo make install
+
+        echo "$fix" >>$file
+        echo
+fi
+
+
 
 echo
 
